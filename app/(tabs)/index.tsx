9117118e51c3b662b943427a2ebbeb9632723ceb
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-08-01 00:24:56 +0530

    2: Setup convex db & functions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,16 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const {toggleDarkMode} = useTheme();
+  const { toggleDarkMode } = useTheme();
+
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos);
+
+  const createTodo = useMutation(api.todos.createTodo);
+  const clearAllTodos = useMutation(api.todos.clearAllTodos);
 
   return (
     <View
@@ -12,6 +20,12 @@ export default function Index() {
       <Text>Hi</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>toogle the mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => createTodo({ text: "walk the dog" })}>
+        <Text>Add a new todo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => clearAllTodos()}>
+        <Text>Clear All</Text>
       </TouchableOpacity>
     </View>
   );
